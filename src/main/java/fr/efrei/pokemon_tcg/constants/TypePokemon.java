6db482f0a48bd45b452acd5921781bf8c15d05ef
Gael <gael.djebar@gmail.com>
2025-01-29 16:37:01 +0100

    Premier commit - ajout des fichiers de base

---
+++ src/main/java/fr/efrei/pokemon_tcg/constants/TypePokemon.java
@@ -0,0 +1,22 @@
+package fr.efrei.pokemon_tcg.constants;
+
+public enum TypePokemon {
+	PLANTE, // 0
+	FEU, // 1
+	EAU, // 2
+	ACIER, // 3
+	PSY, // 4
+	COMBAT, // 5
+	POISON, // 6
+	ROCHE, // 7
+	DRAGON, // 8
+	FEE, // 9
+	ELECTRIQUE, // 10
+	INSECTE, // 11
+	TENEBRES, // 12
+	SOL, // 13
+	VOL, // 14
+	GLACE, // 15
+	NORMAL, // 16
+	SPECTRE // 17
+}
---
+++ src/main/java/fr/efrei/pokemon_tcg/controllers/DresseurController.java
@@ -0,0 +1,95 @@
+package fr.efrei.pokemon_tcg.controllers;
+
+import fr.efrei.pokemon_tcg.constants.TypePokemon;
+import fr.efrei.pokemon_tcg.dto.CapturePokemon;
+import fr.efrei.pokemon_tcg.dto.DresseurDTO;
+import fr.efrei.pokemon_tcg.models.Dresseur;
+import fr.efrei.pokemon_tcg.models.Pokemon;
+import fr.efrei.pokemon_tcg.services.IDresseurService;
+import fr.efrei.pokemon_tcg.services.IPokemonService;
+import fr.efrei.pokemon_tcg.services.implementations.DresseurServiceImpl;
+import fr.efrei.pokemon_tcg.services.implementations.PokemonServiceImpl;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/dresseurs")
+public class DresseurController {
+
+	private final IDresseurService dresseurService;
+	private final IPokemonService pokemonService;
+
+
+	public DresseurController(DresseurServiceImpl dresseurService, PokemonServiceImpl pokemonService) {
+		this.dresseurService = dresseurService;
+		this.pokemonService = pokemonService;
+
+	}
+
+	@GetMapping
+	public ResponseEntity<List<Dresseur>> findAll() {
+		return new ResponseEntity<>(dresseurService.findAll(), HttpStatus.OK);
+	}
+
+	@PostMapping
+	public ResponseEntity<?> create(@RequestBody DresseurDTO dresseurDTO) {
+		dresseurService.create(dresseurDTO);
+		return new ResponseEntity<>(HttpStatus.CREATED);
+	}
+
+	@DeleteMapping("/{uuid}")
+	public ResponseEntity<?> delete(@PathVariable String uuid) {
+		dresseurService.delete(uuid);
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+	}
+
+	@PatchMapping("/{uuid}/capturer")
+	public ResponseEntity<?> capturer(
+			@PathVariable String uuid,
+			@RequestBody CapturePokemon capturePokemon
+	) {
+		dresseurService.capturerPokemon(uuid, capturePokemon);
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+	}
+
+	@PatchMapping("/{uuid}/acheter")
+	public ResponseEntity<?> acheter() {
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+	}
+
+
+	@PatchMapping("/gacha")
+	public ResponseEntity<List<Pokemon>> gacha(@RequestParam(required = false) TypePokemon type) {
+		List<Pokemon> pokemons = pokemonService.findAll(type);
+
+		// Vérifier qu'il y a au moins 5 Pokémon
+		if (pokemons.size() < 5) {
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST); // Retourner une erreur si pas assez de Pokémon
+		}
+
+		// Sélectionner 5 Pokémon au hasard
+		List<Pokemon> randomPokemons = new ArrayList<>();
+		Random rand = new Random();
+
+		// Assurer qu'on n'ajoute pas un Pokémon déjà sélectionné
+		while (randomPokemons.size() < 5) {
+			int randomIndex = rand.nextInt(pokemons.size());
+			Pokemon randomPokemon = pokemons.get(randomIndex);
+
+			// Vérifier si le Pokémon est déjà sélectionné
+			if (!randomPokemons.contains(randomPokemon)) {
+				randomPokemons.add(randomPokemon);
+			}
+		}
+
+		return new ResponseEntity<>(randomPokemons, HttpStatus.OK);
+	}
+
+
+}
